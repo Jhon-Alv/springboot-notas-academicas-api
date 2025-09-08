a94de4f8041403bbbb437c7ab7e9c9a79ad83908
Jhon-Alv <jhon1_alvarado@outlook.com>
2025-09-08 11:41:48 -0500

    enable cors

--- src/main/java/com/practica/notas_academicas/controller/AlumnoController.java
+++ src/main/java/com/practica/notas_academicas/controller/AlumnoController.java
@@ -12,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/alumnos")
+@CrossOrigin(origins = "*")
 public class AlumnoController {
 
     @Autowired
--- src/main/java/com/practica/notas_academicas/controller/CursoController.java
+++ src/main/java/com/practica/notas_academicas/controller/CursoController.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/cursos")
+@CrossOrigin(origins = "*")
 public class CursoController {
 
     @Autowired
